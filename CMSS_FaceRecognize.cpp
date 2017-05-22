6ea65a644049c4623738e02babc0a08dfeb50fe7
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2017-05-22 21:26:12 +0800

    1.0

---
+++ CMSS_FaceRecognize.cpp
@@ -0,0 +1,210 @@
+#include <opencv2/opencv.hpp>
+
+#include "./CMSS_FaceRecognize.h"
+#include "FaceIdentification/include/face_identification.h"
+#include "FaceIdentification/include/recognizer.h"
+#include "FaceIdentification/include/math_functions.h"
+
+#include "FaceDetection/include/face_detection.h"
+#include "FaceAlignment/include/face_alignment.h"
+
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+
+bool check_image_and_convert_to_gray(const cv::Mat& src, cv::Mat& img_gray)
+{
+  if(src.empty())
+    return false;
+  if(!src.data)
+    return false;
+
+  if (src.channels() == 3)
+    cv::cvtColor(src, img_gray, CV_BGR2GRAY);
+  else if (src.channels() == 1)
+    img_gray = src;
+  else {
+    std::cout << "Please input color or gray image.\n";
+    return false;
+  }
+  return true;
+}
+
+bool is_exist(std::string file)
+{
+  std::fstream _file;
+  _file.open(file.c_str(),std::ios::in);
+  if(!_file)
+    {
+      return false;
+    }
+  return true;
+}
+
+int CMSS_FD_GetFaceResult(const cv::Mat& src,
+			  FDPARAM& faceParam,
+			  FDRESULT& faceInfo)
+{
+  cv::Mat img_gray;
+  if (!check_image_and_convert_to_gray(src, img_gray))
+    return -1;
+
+  if (faceParam.pyramid_scale < 0 || faceParam.pyramid_scale > 1)
+    return -2;
+  
+  if (!is_exist(faceParam.modelpath)) {
+    std::cout<<"Detection model: " << faceParam.modelpath << " not exist.\n";
+    return -3;
+  }
+  cmss::FaceDetection detector(faceParam.modelpath);
+  detector.SetMinFaceSize(faceParam.min_face_size);
+  detector.SetMaxFaceSize(faceParam.max_face_size);
+  detector.SetWindowStep(faceParam.slide_wnd_step_x, faceParam.slide_wnd_step_y);
+  detector.SetImagePyramidScaleFactor(faceParam.pyramid_scale);
+  detector.SetScoreThresh(faceParam.score_thresh);
+
+  cmss::ImageData img_data_gray(img_gray.cols, img_gray.rows, img_gray.channels());
+  img_data_gray.data = img_gray.data;
+  std::vector<cmss::FaceInfo> gallery_faces = detector.Detect(img_data_gray);
+  if (gallery_faces.size() == 0) {
+    return 0;
+  } else {
+    FACELOC temp_loc;
+    for (int i = 0; i < gallery_faces.size(); ++i) {
+      temp_loc.faceRect.x = gallery_faces[i].bbox.x;
+      temp_loc.faceRect.y = gallery_faces[i].bbox.y;
+      temp_loc.faceRect.width = gallery_faces[i].bbox.width;
+      temp_loc.faceRect.height = gallery_faces[i].bbox.height;
+      temp_loc.roll = gallery_faces[i].roll;
+      temp_loc.pitch = gallery_faces[i].pitch;
+      temp_loc.yaw = gallery_faces[i].yaw;
+      temp_loc.score = gallery_faces[i].score;
+      faceInfo.push_back(temp_loc);
+    }
+    return 1;
+  }
+}
+
+int CMSS_FA_GetFacePointLocation(const cv::Mat& src,
+				 FDPARAM& faceParam, 
+				 FAPARAM& facePointParam, 
+				 FARESULT& facePointInfo)
+{
+  cv::Mat img_gray;
+  if (!check_image_and_convert_to_gray(src, img_gray))
+    return -1;
+
+  if (!is_exist(faceParam.modelpath)) {
+    std::cout<<"Detection model: " << faceParam.modelpath << " not exist.\n";
+    return -3;
+  }
+
+  cmss::ImageData img_data_gray(img_gray.cols, img_gray.rows, img_gray.channels());
+  img_data_gray.data = img_gray.data;
+
+  // detection
+  FDRESULT face_det;
+  CMSS_FD_GetFaceResult(src, faceParam, face_det);
+  if (face_det.size() == 0) {
+    return 0;
+  }
+  // alignment
+  cmss::FaceAlignment point_detector(facePointParam.modelpath.c_str());
+  for (int i = 0; i < face_det.size(); ++i) {
+    cmss::FaceInfo face_info;
+    face_info.bbox.x = face_det[i].faceRect.x;
+    face_info.bbox.y = face_det[i].faceRect.y;
+    face_info.bbox.width = face_det[i].faceRect.width;
+    face_info.bbox.height = face_det[i].faceRect.height;
+    face_info.roll = face_det[i].roll;
+    face_info.pitch = face_det[i].pitch;
+    face_info.yaw = face_det[i].yaw;
+    face_info.score = face_det[i].score;
+    
+    cmss::FacialLandmark gallery_points[5];
+    point_detector.PointDetectLandmarks(img_data_gray, face_info, gallery_points);
+    FAPIONTLOC face_and_face_point_loc;
+    face_and_face_point_loc.faceLocation = face_det[i];
+    face_and_face_point_loc.facePointNum = 5;
+    for (int j = 0; j < 5; ++j) {
+      cv::Point point(gallery_points[j].x, gallery_points[j].y);
+      face_and_face_point_loc.facePointLocation.push_back(point);
+    }
+    facePointInfo.push_back(face_and_face_point_loc);
+  }
+  return 1;
+}
+
+int CMSS_FR_GetCropFaceandExtraFeature(const cv::Mat& src, 
+				       FDPARAM& faceParam, 
+				       FAPARAM& facePointParam, 
+				       FEPARAM& faceExtrafeaParam, 
+				       CROPRESULT& cropface,
+				       FARESULT& facePointInfo,
+				       FEARESULT&  faceFea)
+{
+  int ret = CMSS_FA_GetFacePointLocation(src, faceParam, facePointParam, facePointInfo);
+  if (ret == -1 || ret == -2 || ret == -3 || ret == 0) {
+    return ret;
+  }
+  if (src.channels() != 3)
+    return -1;
+  cmss::FaceIdentification face_recognizer(faceExtrafeaParam.modelpath.c_str());
+  int crop_width = face_recognizer.crop_width();
+  int crop_height = face_recognizer.crop_height();
+  int channels = 3;
+  int feature_size = face_recognizer.feature_size();
+  cmss::ImageData img_data(src.cols, src.rows, src.channels());
+  img_data.data = src.data;
+  for (int i = 0; i < facePointInfo.size(); ++i) {
+    cmss::FacialLandmark llpoint[5];
+    cmss::ImageData crop_image(crop_width, crop_height, channels);
+    cv::Mat cv_crop_image(crop_height, crop_width, CV_8UC3);
+    crop_image.data = cv_crop_image.data;
+    for (int j = 0; j < 5; ++j) {
+      llpoint[j].x = facePointInfo[i].facePointLocation[j].x;
+      llpoint[j].y = facePointInfo[i].facePointLocation[j].y;
+    }
+    face_recognizer.CropFace(img_data, llpoint, crop_image);
+    cropface.push_back(cv_crop_image);
+  }
+  
+  for (int i = 0; i < cropface.size(); ++i) {
+    std::vector<float> feature (feature_size);
+    cmss::ImageData crop_image(crop_width, crop_height, channels);
+    crop_image.data = cropface[i].data;
+    face_recognizer.ExtractFeature(crop_image, feature.data()); // c++11
+    faceFea.push_back(feature);
+  }
+  return 1;
+}
+
+int CMSS_FR_GetFaceFeatureInfo(FEPARAM& faceExtrafeaParam,
+			       FACEFEAINFO& featureInfo )
+{
+  if (!is_exist(faceExtrafeaParam.modelpath)) {
+    std::cout<<"Recognition model: " << faceExtrafeaParam.modelpath << " not exist.\n";
+    return -3;
+  }
+  cmss::FaceIdentification face_recognizer(faceExtrafeaParam.modelpath.c_str());
+  featureInfo.featureSize = face_recognizer.feature_size();
+  featureInfo.cropfaceWidth = face_recognizer.crop_width();
+  featureInfo.cropfaceHeight = face_recognizer.crop_height();
+  return 0;
+}
+
+float CMSS_FR_CalcSimilarity (float* fea1,
+			      float* fea2,
+			      int len)
+{
+  if (len < 1) {
+    std::cout << "Please provide feature size\n";
+    return -1;
+  }
+  return simd_dot(fea1, fea2, len)
+	  / (sqrt(simd_dot(fea1, fea1, len))
+	  * sqrt(simd_dot(fea2, fea2, len)));
+}
---
+++ test_main.cpp
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "CMSS_FaceRecognize.h"
+
+void plot_rect(cv::Mat img_rect, FDRESULT faceInfo)
+{
+  for (int i = 0; i < faceInfo.size(); ++i) {
+    cv::rectangle(img_rect, cv::Point(faceInfo[i].faceRect.x, faceInfo[i].faceRect.y),
+		  cv::Point(faceInfo[i].faceRect.x + faceInfo[i].faceRect.width,
+			    faceInfo[i].faceRect.y + faceInfo[i].faceRect.height),
+		  cv::Scalar(255, 0, 0), 3);
+  }
+  return;
+}
+
+int main(int argc, char *argv[])
+{
+  std::string img1_file = "../1.jpg";
+  std::string img2_file = "../2.jpg";
+  // std::string img1_file = "../Aaron_Peirsol_0001.jpg";
+  // std::string img2_file = "../Aaron_Peirsol_0004.jpg";
+
+  std::string det_model = "../cmssFaceEngine/FaceDetection/model/detection.model";
+  std::string align_model = "../cmssFaceEngine/FaceAlignment/model/alignment.model";
+  std::string id_model = "../cmssFaceEngine/FaceIdentification/model/recognition.model";
+
+  cv::Mat img1 = cv::imread(img1_file.c_str());
+  cv::Mat img2 = cv::imread(img2_file.c_str());
+  // ****************************************
+  // test detection
+  // ****************************************
+  FDPARAM faceParam = {20, -1, 4, 4, 0.8, 2.0, const_cast< char* >(det_model.c_str())};
+  FDRESULT faceInfo;
+  FDRESULT faceInfo2;
+  CMSS_FD_GetFaceResult(img1, faceParam, faceInfo);
+  CMSS_FD_GetFaceResult(img2, faceParam, faceInfo2);
+  cv::Mat img_rect = img1.clone();
+  cv::Mat img_rect2 = img2.clone();
+  if (faceInfo.size() < 1 || faceInfo2.size() < 1) {
+    std::cout << "Detect no face.\n";
+    return 0;
+  } else {
+    plot_rect(img_rect, faceInfo);
+    cv::imshow("detection", img_rect);
+    plot_rect(img_rect2, faceInfo2);
+    cv::imshow("detection2", img_rect2);
+    cv::waitKey(0);
+  }
+
+  // ****************************************
+  // test alignment
+  // ****************************************
+  FAPARAM facePointParam = {const_cast< char* >(align_model.c_str())};
+  FARESULT facePointInfo;
+  FARESULT facePointInfo2;
+  CMSS_FA_GetFacePointLocation(img1, faceParam, facePointParam, facePointInfo);
+  CMSS_FA_GetFacePointLocation(img2, faceParam, facePointParam, facePointInfo2);
+  
+  cv::Mat img_point = img1.clone();
+  cv::Mat img_point2 = img2.clone();
+  for (int j = 0; j < facePointInfo.size(); ++j) {
+    for (int i = 0; i<5; i++) {
+	cv::circle(img_point, cv::Point(facePointInfo[j].facePointLocation[i].x,
+					facePointInfo[j].facePointLocation[i].y),
+		   2, CV_RGB(0, 255, 0));
+	cv::circle(img_point2, cv::Point(facePointInfo2[j].facePointLocation[i].x,
+					facePointInfo2[j].facePointLocation[i].y),
+		   2, CV_RGB(0, 255, 0));
+    }
+  }
+  cv::imshow("alignment", img_point);
+  cv::imshow("alignment2", img_point2);
+  cv::waitKey(0);
+
+  // ****************************************
+  // test crop and extract feature
+  // ****************************************
+  FEPARAM faceExtrafeaParam = {const_cast<char *>(id_model.c_str())};
+  CROPRESULT cropface;
+  FEARESULT faceFea;
+  CROPRESULT cropface2;
+  FEARESULT faceFea2;
+  CMSS_FR_GetCropFaceandExtraFeature(img1, faceParam, facePointParam,
+				     faceExtrafeaParam, cropface,
+				     facePointInfo, faceFea);
+  CMSS_FR_GetCropFaceandExtraFeature(img2, faceParam, facePointParam,
+				     faceExtrafeaParam, cropface2,
+				     facePointInfo2, faceFea2);
+
+  if (cropface.size() < 1 || cropface2.size() < 1) {
+    std::cout << "Crop faces failed\n";
+    return -1;
+  }
+  cv::imshow("crop_face", cropface[0]);
+  cv::imshow("crop_face2", cropface2[0]);
+  cv::waitKey(0);
+
+  // ****************************************
+  // get feature info
+  // ****************************************
+  FACEFEAINFO featureInfo;
+  CMSS_FR_GetFaceFeatureInfo(faceExtrafeaParam, featureInfo);
+  printf("\nfeature_size[%d], crop_width[%d], crop_height[%d]\n\n",
+	 featureInfo.featureSize, featureInfo.cropfaceWidth, featureInfo.cropfaceHeight);
+  
+  // ****************************************
+  // caculate similarity
+  // ****************************************
+  std::ofstream of_fea("test_main.txt");  
+  of_fea << img1_file << "\n";
+  for (int i = 0; i < 2048; ++i) {
+    of_fea << faceFea[0][i] << "\n";
+  }
+  of_fea << "\n" << img2_file << "\n";
+  for (int i = 0; i < 2048; ++i) {
+    of_fea << faceFea2[0][i] << "\n";
+  }
+  of_fea << "\n";
+  of_fea.close();
+
+  float sim = CMSS_FR_CalcSimilarity(faceFea[0].data(), faceFea2[0].data(),
+				     featureInfo.featureSize);
+  std::cout << "Similarity : " << sim << std::endl;
+  return 0;
+}
